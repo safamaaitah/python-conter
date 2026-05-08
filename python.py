4afb69c555ddf129a42278f05e048918e9f0d6fc
safamaaitah <safa2002125@gmail.com>
2026-05-08 11:06:48 -0900

    first commit : python app counter

---
+++ python.py
@@ -0,0 +1,42 @@
+# data=10
+# print(type(data))
+# data2="n3ab8io"
+# print(type(data2))
+# a="10.5"
+# b=0
+# b=float(a)
+# b=int(a)
+# print(a," ",b)
+
+# text="python programing"
+# print(text[0:6])
+# print(text[-11:])
+
+# name="safa"
+# scour=90
+# masseg=f"user is: {name} achived a scoure: {scour}"
+# print(masseg)
+
+# str1="Londen"
+# print(str1.upper())
+# print(str1.lower())
+# str2="apple"
+# print(len(str2))
+# print(str1.find("l"))
+
+# n1="safa"
+# n2="s"
+# n3="safa maaitah"
+# print(n1.count("safa"))
+
+# print(bool(""))
+# n=int(input())
+# print(f"5 + {n}")
+
+character=input("enter character: ")
+word=input("enter word : ")
+sentece=input("enter sentece :")
+c=len(character)
+w=len(word)
+s=len(sentece.replace(" ",""))
+print(f" the number of character is {c} , the number of word is {w} ,the number of sentect is {s} ")
